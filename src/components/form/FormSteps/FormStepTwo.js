797eb22f13a018db390ecae772fc7cdbe11b563b
justin1494 <m.jaskolowski1994@gmail.com>
2022-12-28 13:57:36 +0100

    addons price change when yearly plan is choseb

--- src/components/form/FormSteps/FormStepTwo.js
+++ src/components/form/FormSteps/FormStepTwo.js
@@ -6,268 +6,262 @@ import { ReactComponent as IconPro } from "../../../assets/images/icon-pro.svg";
 import { fadeIn } from "react-animations";
 
 const initialPlans = [
-	{
-		id: 0,
-		title: "Arcade",
-		price: "$9/",
-		img: <IconArcade />,
-		selected: false,
-	},
-	{
-		id: 1,
-		title: "Advanced",
-		price: "$12/",
-		img: <IconAdvanced />,
-		selected: false,
-	},
-	{
-		id: 2,
-		title: "Pro",
-		price: "$15/",
-		img: <IconPro />,
-		selected: false,
-	},
+  {
+    id: 0,
+    title: "Arcade",
+    price: "$9",
+    img: <IconArcade />,
+    selected: false,
+  },
+  {
+    id: 1,
+    title: "Advanced",
+    price: "$12",
+    img: <IconAdvanced />,
+    selected: false,
+  },
+  {
+    id: 2,
+    title: "Pro",
+    price: "$15",
+    img: <IconPro />,
+    selected: false,
+  },
 ];
 
 const fadeInAnimation = keyframes`${fadeIn}`;
 
-function FormStepTwo() {
-	const [switchActivate, setSwitchActivate] = useState(false);
-	const [plans, setPlans] = useState(initialPlans);
+function FormStepTwo({ switchActivate, setSwitchActivate }) {
+  const [plans, setPlans] = useState(initialPlans);
 
+  const handlePlanSelect = (planId) => {
+    setPlans(
+      plans.map((plan) => {
+        if (plan.id === planId) {
+          return { ...plan, selected: true };
+        }
+        return { ...plan, selected: false };
+      })
+    );
+  };
 
-	const handlePlanSelect = (planId) => {
-		setPlans(
-			plans.map((plan) => {
-				if (plan.id === planId) {
-					return { ...plan, selected: true };
-				}
-				return { ...plan, selected: false };
-			})
-		);
-	};
+  const handleSwitch = () => {
+    setSwitchActivate(!switchActivate);
+  };
 
-	const handleSwitch = () => {
-		setSwitchActivate(!switchActivate);
-	};
+  return (
+    <StyledFormStepOne>
+      <h1>Select Plan</h1>
+      <p className="subtitle">
+        You have the option of monthly or yearly billing.
+      </p>
 
-	return (
-		<StyledFormStepOne>
-			<h1>Select Plan</h1>
-			<p className="subtitle">
-				You have the option of monthly or yearly billing.
-			</p>
-
-			<div className="plans">
-				{plans.map((plan, i) => (
-					<div
-						key={i}
-						className={`plan ${plan.selected && "selected"} ${
-							switchActivate && "plan-animate"
-						}`}
-						onClick={() => {
-							handlePlanSelect(plan.id);
-						}}>
-						<div className="plan-img">{plan.img}</div>
-						<div className="plan-description">
-							<div className="plan-title">{plan.title}</div>
-							<div className="plan-price">{`${
-								plan.price
-							}${switchActivate ? "yr" : "mo"}`}</div>
-							{switchActivate && (
-								<div className="plan-yearly">
-									2 months free
-								</div>
-							)}
-						</div>
-					</div>
-				))}
-			</div>
-			<div className="plan-length">
-				<p className={`monthly ${!switchActivate && "activated"}`}>
-					Monthly
-				</p>
-				<div className="switch" onClick={handleSwitch}>
-					<div
-						className={
-							switchActivate
-								? "switch-ball switch-ball-activate"
-								: "switch-ball"
-						}></div>
-				</div>
-				<p className={`yearly ${switchActivate && "activated"}`}>
-					Yearly
-				</p>
-			</div>
-		</StyledFormStepOne>
-	);
+      <div className="plans">
+        {plans.map((plan, i) => (
+          <div
+            key={i}
+            className={`plan ${plan.selected && "selected"} ${
+              switchActivate && "plan-animate"
+            }`}
+            onClick={() => {
+              handlePlanSelect(plan.id);
+            }}
+          >
+            <div className="plan-img">{plan.img}</div>
+            <div className="plan-description">
+              <div className="plan-title">{plan.title}</div>
+              <div className="plan-price">{`${plan.price}${
+                switchActivate ? "0/yr" : "/mo"
+              }`}</div>
+              {switchActivate && (
+                <div className="plan-yearly">2 months free</div>
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="plan-length">
+        <p className={`monthly ${!switchActivate && "activated"}`}>Monthly</p>
+        <div className="switch" onClick={handleSwitch}>
+          <div
+            className={
+              switchActivate
+                ? "switch-ball switch-ball-activate"
+                : "switch-ball"
+            }
+          ></div>
+        </div>
+        <p className={`yearly ${switchActivate && "activated"}`}>Yearly</p>
+      </div>
+    </StyledFormStepOne>
+  );
 }
 
 const StyledFormStepOne = styled.div`
-	height: 100%;
+  height: 100%;
 
-	h1 {
-		margin-bottom: 0.5rem;
-		color: hsl(213, 96%, 18%);
-	}
-	.subtitle {
-		margin-bottom: 2rem;
-		color: hsl(231, 11%, 63%);
-		font-size: 16px;
-		font-weight: 400;
-	}
+  h1 {
+    margin-bottom: 0.5rem;
+    color: hsl(213, 96%, 18%);
+  }
+  .subtitle {
+    margin-bottom: 2rem;
+    color: hsl(231, 11%, 63%);
+    font-size: 16px;
+    font-weight: 400;
+  }
 
-	.plans {
-		display: flex;
-		justify-content: space-between;
+  .plans {
+    display: flex;
+    justify-content: space-between;
 
-		.plan {
-			height: 180px;
-			width: 30%;
-			display: flex;
-			justify-content: space-between;
-			flex-direction: column;
-			padding: 1rem;
-			border-radius: 0.5rem;
-			border: 1px solid hsl(229, 24%, 87%);
-			cursor: pointer;
+    .plan {
+      height: 180px;
+      width: 30%;
+      display: flex;
+      justify-content: space-between;
+      flex-direction: column;
+      padding: 1rem;
+      border-radius: 0.5rem;
+      border: 1px solid hsl(229, 24%, 87%);
+      cursor: pointer;
 
-			:hover {
-				border: 1px solid hsl(213, 96%, 18%);
-			}
-		}
-		.selected {
-			background-color: hsl(217, 100%, 97%);
-			border: 1px solid hsl(213, 96%, 18%);
-		}
-	}
+      :hover {
+        border: 1px solid hsl(213, 96%, 18%);
+      }
+    }
+    .selected {
+      background-color: hsl(217, 100%, 97%);
+      border: 1px solid hsl(213, 96%, 18%);
+    }
+  }
 
-	.plan-img {
-		margin-bottom: 2rem;
-	}
+  .plan-img {
+    margin-bottom: 2rem;
+  }
 
-	.plan-title {
-		margin-bottom: 0.2rem;
-		color: hsl(213, 96%, 18%);
-		font-weight: 500;
-		font-size: 17px;
-	}
+  .plan-title {
+    margin-bottom: 0.2rem;
+    color: hsl(213, 96%, 18%);
+    font-weight: 500;
+    font-size: 17px;
+  }
 
-	.plan-price {
-		color: hsl(231, 11%, 63%);
-		margin: 0.5rem 0;
-	}
+  .plan-price {
+    color: hsl(231, 11%, 63%);
+    margin: 0.5rem 0;
+  }
 
-	.plan-yearly {
-		color: hsl(213, 96%, 18%);
-		letter-spacing: -0.5px;
-		font-weight: 500;
-		animation: 0.3s ${fadeInAnimation};
-	}
+  .plan-yearly {
+    color: hsl(213, 96%, 18%);
+    letter-spacing: -0.5px;
+    font-weight: 500;
+    animation: 0.3s ${fadeInAnimation};
+  }
 
-	img {
-		width: 100px;
-		height: 100px;
-	}
+  img {
+    width: 100px;
+    height: 100px;
+  }
 
-	.plan-length {
-		width: 100%;
-		height: 50px;
-		margin-top: 2rem;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		border-radius: 0.5rem;
-		background-color: hsl(217, 100%, 97%);
+  .plan-length {
+    width: 100%;
+    height: 50px;
+    margin-top: 2rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 0.5rem;
+    background-color: hsl(217, 100%, 97%);
 
-		p {
-			display: flex;
-			align-items: center;
-			width: 100px;
-			margin: 0 1rem;
-			color: hsl(231, 11%, 63%);
-		}
+    p {
+      display: flex;
+      align-items: center;
+      width: 100px;
+      margin: 0 1rem;
+      color: hsl(231, 11%, 63%);
+    }
 
-		.monthly {
-			justify-content: flex-end;
-		}
-		.activated {
-			font-weight: 500;
-			color: hsl(213, 96%, 18%) !important;
-			transition: color 0.3s;
-		}
+    .monthly {
+      justify-content: flex-end;
+    }
+    .activated {
+      font-weight: 500;
+      color: hsl(213, 96%, 18%) !important;
+      transition: color 0.3s;
+    }
 
-		.switch {
-			display: flex;
-			justify-content: flex-start;
-			align-items: center;
-			width: 50px;
-			height: 50%;
-			border-radius: 50px;
-			background-color: hsl(213, 96%, 18%);
-			cursor: pointer;
+    .switch {
+      display: flex;
+      justify-content: flex-start;
+      align-items: center;
+      width: 50px;
+      height: 50%;
+      border-radius: 50px;
+      background-color: hsl(213, 96%, 18%);
+      cursor: pointer;
 
-			.switch-ball {
-				height: 63%;
-				margin: 0 0.3rem;
-				aspect-ratio: 1/1;
-				border-radius: 50%;
-				background-color: #fff;
-				transition: all 0.5s;
-			}
-		}
-		.switch-ball-activate {
-			transform: translateX(25px);
-		}
-	}
+      .switch-ball {
+        height: 63%;
+        margin: 0 0.3rem;
+        aspect-ratio: 1/1;
+        border-radius: 50%;
+        background-color: #fff;
+        transition: all 0.5s;
+      }
+    }
+    .switch-ball-activate {
+      transform: translateX(25px);
+    }
+  }
 
-	@media (max-width: 640px) {
-		background-color: #fff;
+  @media (max-width: 640px) {
+    background-color: #fff;
 
-		p {
-			margin-bottom: 1rem;
-		}
+    p {
+      margin-bottom: 1rem;
+    }
 
-		.subtitle {
-			margin-bottom: 1rem;
-		}
-		.plans {
-			flex-direction: column;
-			gap: 0.5rem;
+    .subtitle {
+      margin-bottom: 1rem;
+    }
+    .plans {
+      flex-direction: column;
+      gap: 0.5rem;
 
-			.plan {
-				height: 85px;
-				width: 100%;
-				justify-content: flex-start;
-				flex-direction: row;
-				gap: 1rem;
+      .plan {
+        height: 85px;
+        width: 100%;
+        justify-content: flex-start;
+        flex-direction: row;
+        gap: 1rem;
 
-				.plan-title,
-				.plan-price,
-				.plan-yearly {
-					font-size: 14px;
-				}
-				.plan-title {
-					margin-bottom: 0.1rem;
-				}
+        .plan-title,
+        .plan-price,
+        .plan-yearly {
+          font-size: 14px;
+        }
+        .plan-title {
+          margin-bottom: 0.1rem;
+        }
 
-				.plan-price {
-					margin: 0;
-				}
+        .plan-price {
+          margin: 0;
+        }
 
-				.plan-img {
-					margin-bottom: 0;
-				}
-				.plan-yearly {
-					margin-top: 0.1rem;
-				}
-			}
-		}
+        .plan-img {
+          margin-bottom: 0;
+        }
+        .plan-yearly {
+          margin-top: 0.1rem;
+        }
+      }
+    }
 
-		.plan-length {
-			margin-top: 1rem;
-		}
-	}
+    .plan-length {
+      margin-top: 1rem;
+    }
+  }
 `;
 
 export default FormStepTwo;
